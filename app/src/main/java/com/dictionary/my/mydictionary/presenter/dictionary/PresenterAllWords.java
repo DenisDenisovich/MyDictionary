1b81de4b4df157b239e057c60aef0dffc7c203ec
vlad denisov <luxsorr7@gmail.com>
2018-03-11 00:58:34 +0300

    created presenter and view interfaces for AllWords

---
+++ app/src/main/java/com/dictionary/my/mydictionary/presenter/dictionary/PresenterAllWords.java
@@ -0,0 +1,8 @@
+package com.dictionary.my.mydictionary.presenter.dictionary;
+
+/**
+ * Created by luxso on 10.03.2018.
+ */
+
+public interface PresenterAllWords {
+}
---
+++ app/src/main/java/com/dictionary/my/mydictionary/presenter/dictionary/impl/PresenterAllWordsImpl.java
@@ -0,0 +1,10 @@
+package com.dictionary.my.mydictionary.presenter.dictionary.impl;
+
+import com.dictionary.my.mydictionary.presenter.dictionary.PresenterAllWords;
+
+/**
+ * Created by luxso on 11.03.2018.
+ */
+
+public class PresenterAllWordsImpl implements PresenterAllWords {
+}
